c09082757f8000184acb9f094b30983ef2bef5af
Casey Chu <casey@bitsofpancake.com>
2013-05-10 16:48:30 -0700

    button acts more consistently

--- loop-youtube-video.user.js
+++ loop-youtube-video.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name			Loop YouTube Video
-// @description		Adds a Loop button that lets you auto-repeat a YouTube video.
+// @description		Adds a Loop button to YouTube that lets you auto-repeat videos.
 // @namespace		loop-youtube-video-c
 // @match			*://*.youtube.com/watch?v=*
-// @version			2.0
+// @version			2.1
 // ==/UserScript==
 
 // Make sure we're actually on a video page!
-if (!document.getElementById('watch-like-dislike-buttons'))
+if (!document.getElementById('watch7-sentiment-actions'))
 	return;
 	
 // Add the button to toggle whether we're looping.
@@ -25,7 +25,29 @@ button.innerHTML =
 		role="button"> \
 			<span class="yt-uix-button-content">Loop </span> \
 	</button>';
-document.getElementById('watch-like-dislike-buttons').appendChild(button);
+document.getElementById('watch7-sentiment-actions').appendChild(button);
+
+// Styles for the button. Most of it is taken from YouTube's style for 
+// .yt-uix-button-panel:hover #watch-like-dislike-buttons .yt-uix-button-text.yt-uix-button-toggled
+GM_addStyle('\
+	.yt-uix-button-toggled#watch-loop { \
+		color: #932720; \
+	} \
+	.yt-uix-button-panel:hover .yt-uix-button-toggled#watch-loop { \
+		border-color: #c6c6c6; \
+		background-color: #e9e9e9; \
+		-moz-box-shadow: inset 0 1px 1px rgba(0, 0, 0, .20); \
+		-ms-box-shadow: inset 0 1px 1px rgba(0, 0, 0, .20); \
+		-webkit-box-shadow: inset 0 1px 1px rgba(0, 0, 0, .20); \
+		box-shadow: inset 0 1px 1px rgba(0, 0, 0, .20); \
+		filter: progid:DXImageTransform.Microsoft.Gradient(GradientType=0, StartColorStr=#fff8f8f8, EndColorStr=#ffeeeeee); \
+		background-image: -moz-linear-gradient(top, #f8f8f8 0, #eee 100%); \
+		background-image: -ms-linear-gradient(top, #f8f8f8 0, #eee 100%); \
+		background-image: -o-linear-gradient(top, #f8f8f8 0, #eee 100%); \
+		background-image: -webkit-gradient(linear, left top, left bottom, color-stop(0, #f8f8f8), color-stop(100%, #eee)); \
+		background-image: -webkit-linear-gradient(top, #f8f8f8 0, #eee 100%); \
+		background-image: linear-gradient(to bottom, #f8f8f8 0, #eee 100%); \
+	}');
 
 // Injects code into the global scope.
 function inject(fn) {
@@ -44,15 +66,8 @@ inject(function () {
 	// Listen for the button click.
 	document.getElementById('watch-loop').onclick = function () {
 		looping = !looping;
-		
-		// Update all the UI
-		if (looping) {
-			this.setAttribute('data-tooltip-text', 'Turn off looping');
-			this.style.color = '#932720';
-		} else {
-			this.setAttribute('data-tooltip-text', 'Loop this video');
-			this.style.color = '';
-		}
+		this.setAttribute('data-tooltip-text', looping ? 'Turn off looping' : 'Loop this video');
+		return false;
 	};
 		
 	// Returns the player API interface or false if it's not ready.
